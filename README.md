ffa699a87554485d3618f52562482f64f455bbd5
100451296 <98945796+100451296@users.noreply.github.com>
2024-03-03 22:16:46 +0100

    avanves varios

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PL-1
+
+
+## Dudas
+1. Letras minúsculas/mayúscula en hexadecimal
--- main.py
+++ main.py
@@ -3,21 +3,32 @@ import ply.yacc as yacc
 
 # Lista de tokens
 tokens = (
-    "INTEGER",  # Números enteros
-    "FLOAT",  # Números reales
-    "SCIENTIFIC",  # Notación científica
-    "BINARY",  # Números binarios
-    "OCTAL",  # Números octales
+    "INTEGER",  # Numeros enteros
+    "FLOAT",  # Numeros reales
+    "SCIENTIFIC",  # Notacion cientifica
+    "BINARY",  # Numeros binarios
+    "OCTAL",  # Numeros octales
+    "HEX",  # Numeros hexadecimales
+    "STRING",  # Cadena sin comillas (solo para claves)
+    "QUOTED_STRING",  # Cadena con comillas
+    "TRUE",  # Booleano True (caracter reservado)
+    "FALSE",  # Booleano False (caracter reservado)
+    "NULL",  # NULL (caracter reservado)
 )
 
 # Definición de las expresiones regulares para los tokens
 t_INTEGER = r"-?\d+"
 t_FLOAT = r"-?(\d*\.\d+)|(\d+\.\d*)"
-t_SCIENTIFIC = r"(\d*(\.\d+)?){1}[eE][+-]?\d+"
-
-
+t_SCIENTIFIC = r"(\d+(\.\d*)?|\.\d+)[eE][+-]?\d+"
 t_BINARY = r"0[bB][01]+"
 t_OCTAL = r"0[0-7]+"
+t_HEX = r"0[xX][0-9A-F]+"
+t_STRING = r"(?!TR\b|tr\b|FL\b|fl\b|NULL\b|null\b)[_a-zA-Z][_a-zA-Z0-9]*\b"
+t_QUOTED_STRING = r'"([^"\n]*)?"'
+t_TRUE = r"TR|tr"
+t_FALSE = r"FL|fl"
+t_NULL = r"NULL|null"
+
 
 # Ignorar espacios y tabulaciones
 t_ignore = " \t"
@@ -33,7 +44,9 @@ def t_error(t):
 lexer = lex.lex()
 
 # Test del analizador léxico
-data = "10 420 -12 -999 0.1289 .12 -100.001 12. 10e-1 E10 5E2 4e-2 0b101 0B110110 0712 0332 01121"
+data = '10 420 -12 -999 0.1289 .12 -100.001 12. 10e-1 10.1E10 1.E10 5E2 .1.1E10  E54 4e-2 0b101 0B110110 B0 0b 0b010 0712 0332 01121 \
+0xA0F 0X0AF _a0 a_0 Aa 0_a "hola" "" TR FL tr fl TREX trex flex "TR EX" null NULL nulla '
+
 
 lexer.input(data)
 while True:
